45c1472165f12eee7969d5ee4a37512f7ce9a62a
Michael Edelmann <michael@medelmann.de>
2023-04-28 17:15:35 +0200

    fix typo in primary key

--- server/create-database.sql
+++ server/create-database.sql
@@ -5,10 +5,10 @@ create table tasks (
   location varchar,
   start_date date,
   start_time time
- );
+);
  
  create table next_task_map (
   task_id int not null references tasks(id),
   next_task_id int not null references tasks(id),
-  primary key (task_id, previous_task_id)
- );
+  primary key (task_id, next_task_id)
+);
