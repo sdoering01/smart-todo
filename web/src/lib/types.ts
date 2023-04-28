dff93d1acec8677b8738e404a2574d21d15fed61
sdoering01 <57443123+sdoering01@users.noreply.github.com>
2023-04-28 18:14:35 +0200

    add expected types for api interaction

---
+++ web/src/lib/types.ts
@@ -0,0 +1,14 @@
+export type ApiTask = {
+    id: number;
+    title: string;
+    description?: string;
+    date?: string;  // yyyy-mm-dd (https://en.wikipedia.org/wiki/ISO_8601)
+    time?: string;  // hh:mm
+    nextTaskIds: number[];
+}
+
+// Like ApiTask, but without id (since it's unknown at this point) and with previousTaskIds
+export type ApiCreateTask = Omit<ApiTask, 'id'> & { previousTaskIds: number[] };
+
+// Like ApiCreateTask, but may include only some of the fields
+export type ApiUpdateTask = Partial<ApiCreateTask>;
