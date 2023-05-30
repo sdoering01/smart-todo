d772a7b15f0434886a773849a6a7eb9ef2e83a37
sdoering01 <57443123+sdoering01@users.noreply.github.com>
2023-05-30 19:50:17 +0200

    fix CORS on server

--- server/src/server.go
+++ server/src/server.go
@@ -503,7 +503,7 @@ func corsMiddleware(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Access-Control-Allow-Origin", "*")
 		w.Header().Add("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE")
-		w.Header().Add("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
 			return
